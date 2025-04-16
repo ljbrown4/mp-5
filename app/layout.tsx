fae92b6f00cc9174e41ce33172d3941f42b9ccc2
leigh <ljbrown4@bu.edu>
2025-04-16 15:39:15 -0400

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,17 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Inter, Roboto, DM_Sans } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
 });
 
-const inter = Inter({
-    variable: "--font-inter",
-    subsets: ["latin"],
-    weight: ["400", "700"],
-});
-const roboto = Roboto({
-    variable: "--font-roboto",
-    subsets: ["latin"],
-    weight: ["400", "700"],
-});
-
-const dmSans = DM_Sans({
-    variable: "--font-dm-sans",
-    subsets: ["latin"],
-    weight: ["400", "700"],
-});
 
 export const metadata: Metadata = {
   title: "mp-5 URL Shortener",
@@ -35,16 +19,17 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable}  ${inter.variable} ${roboto.variable} ${dmSans.variable} antialiased`}>
+    return (
+        <html lang="en">
+        <body
+            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
         {children}
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
