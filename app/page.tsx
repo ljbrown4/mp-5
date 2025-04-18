89483aed309f663abd2416177a24b39a0a4608b2
leigh <ljbrown4@bu.edu>
2025-04-17 21:40:27 -0400

    .

--- app/page.tsx
+++ app/page.tsx
@@ -29,14 +29,15 @@ export default function Home() {
         <ThemeProvider theme={theme}>
             <main className="min-h-screen w-full flex-col justify-center bg-[#c5c2c9]">
                 <Header />
-                <div className="flex flex-row flex-wrap justify-center items-center w-full">
-                    <div className="w-[50%]">
+                <div className="flex flex-col md:flex-row justify-center items-center w-full">
+                    {/*looked up how to make it change between flex row and col based on screen size*/}
+                    <div className=" w-[80%] md:w-[50%]">
                         <h1 className="font-bold text-4xl text-[#453750] text-center mt-4 p-2">URL SHORTENER</h1>
                         <p className="font-bold text-xl text-[#73648a] text-center mb-2 p-2">
                             Aliases can not be duplicated. Please delete old alias in the Saved URLs page. </p>
                         <URLShortenerForm append={() => {}} />
                     </div>
-                    <div className="w-[45%]">
+                    <div className="w-[80%] md:w-[45%]">
                         <h2 className="font-bold w-full text-2xl text-[#453750] mb-4 mt-6 text-center">
                             Favourited URLs
                         </h2>
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,19 +1,22 @@
 import Link from "next/link";
 
 export default function Header() {
-    const linkStyling = "p-1 m-2 text-xl font-bold hover:underline";
-    const headerStyle = "w-full flex justify-between items-center h-25 text-[#618f63] bg-[whitesmoke]";
+    const linkStyling = "md:p-1 mb-2 md:m-2 text-lg md:text-xl font-bold hover:underline text-[#4c7157]";
+    const headerStyle = "w-full flex flex-wrap justify-between items-center p-3 text-[#618f63] bg-[whitesmoke]";
+
     return (
         <header className={headerStyle}>
-            <div className="flex-col items-center justify-between ">
-                <h2 className = "text-4xl font-semibold p-1 ml-2 mb-1"> CS-391 URL SHORTENER </h2>
-                <p className="ml-2 font-bold"> shorten long urls into easy to understand and user-friendly aliases </p>
+            <div className="flex flex-col items-start mb-4 md:mb-0">
+                <h2 className="text-2xl md:text-4xl font-semibold">CS-391 URL SHORTENER</h2>
+                <p className="text-sm md:text-base font-bold">
+                    shorten long urls into easy to understand and user-friendly aliases
+                </p>
             </div>
-            <nav className = "p-2 m-4 flex">
-                <Link href="/" className = {linkStyling}>
+            <nav className="flex flex-wrap space-x-4">
+                <Link href="/" className={linkStyling}>
                     Home
                 </Link>
-                <Link href="/saved-urls" className = {linkStyling}>
+                <Link href="/saved-urls" className={linkStyling}>
                     Saved URLs
                 </Link>
             </nav>
